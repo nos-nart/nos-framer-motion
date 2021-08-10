019ba4e418e9cf99fbabb9a3ae36cd9332550f14
nos-nart <nosnart1510@gmail.com>
2021-08-10 19:55:58 +0700

    [+] ff

---
+++ framer-page-transition/src/pages/home.jsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import ProgressiveImage from "react-progressive-image";
+import yasmeen from "../images/yasmeen.webp";
+import compressImage from "../images/compressed-image.jpg";
+
+const Home = ({ imageDetails, image }) => (
+  <>
+    <main>
+      <div className='container'>
+        <div className='row center'>
+          <div className='image-container'>
+            <div
+              className='thumbnail'
+              ref={image}
+              style={{
+                width: imageDetails.width,
+                height: imageDetails.height,
+              }}>
+              <div className='frame'>
+                <Link to={`/model/yasmeen-tariq`}>
+                  <ProgressiveImage
+                    src={yasmeen}
+                    placeholder={compressImage}>
+                    {(src) => <img src={src} alt='Yasmeen Tariq' />}
+                  </ProgressiveImage>
+                </Link>
+              </div>
+            </div>
+            <div className='information'>
+              <div className='title'>Yasmeen Tariq</div>
+              <div className='location'>
+                <span>28.538336</span>
+                <span>-81.379234</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </>
+);
+
+export default Home;
