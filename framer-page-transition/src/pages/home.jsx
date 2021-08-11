717ab85950f724251585d94e1618ad5ed11b90d4
nos-nart <nosnart1510@gmail.com>
2021-08-11 23:25:45 +0700

    [+] aa

--- framer-page-transition/src/pages/home.jsx
+++ framer-page-transition/src/pages/home.jsx
@@ -1,8 +1,11 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import ProgressiveImage from "react-progressive-image";
 import yasmeen from "../images/yasmeen.webp";
 import compressImage from "../images/compressed-image.jpg";
+
+const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
 
 const Home = ({ imageDetails, image }) => (
   <>
@@ -22,18 +25,28 @@ const Home = ({ imageDetails, image }) => (
                   <ProgressiveImage
                     src={yasmeen}
                     placeholder={compressImage}>
-                    {(src) => <img src={src} alt='Yasmeen Tariq' />}
+                    {(src) => (
+                      <motion.img
+                        src={src}
+                        alt='Yasmeen Tariq'
+                        whileHover={{ scale: 1.1 }}
+                        transition={transition}
+                      />
+                    )}
                   </ProgressiveImage>
                 </Link>
               </div>
             </div>
-            <div className='information'>
+            <motion.div
+              exit={{ opacity: 0 }}
+              transition={transition}
+              className='information'>
               <div className='title'>Yasmeen Tariq</div>
               <div className='location'>
                 <span>28.538336</span>
                 <span>-81.379234</span>
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
